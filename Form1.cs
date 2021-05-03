210887055e415940be0157c477405aedfc515e9b
Emilio <brianemilio14oc@gmail.com>
2021-05-03 13:00:21 -0600

    Mi Primer Commit

---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Ingrese un valor para cadena 1");
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Ingrese un valor para cadena 2");
+            }
+            if (string.IsNullOrEmpty(textBox9.Text))
+            {
+                MessageBox.Show("Ingrese el algun valor para potencia");
+            }
+            else
+            {
+                string low = textBox1.Text.ToLower();
+                low = low.Replace(" ", "");
+
+
+                string cadena1 = textBox1.Text.Replace(" ", "");
+                string cadena2 = textBox2.Text.Replace(" ", "");
+                textBox3.Text = textBox1.Text + textBox2.Text;
+                textBox5.Text = Convert.ToString(cadena1.Length);
+                textBox6.Text = Convert.ToString(cadena2.Length);
+                textBox7.Text = Convert.ToString(cadena1.Length + cadena2.Length);
+                if (cadena1 == cadena2)
+                {
+                    textBox4.Text = "Son iguales";
+                }
+                else
+                {
+                    textBox4.Text = "No son iguales";
+                }
+
+                string repetida = "";
+                for (int indice = 0; indice < Convert.ToInt32(textBox9.Text); indice++)
+                {
+                    repetida += cadena1;
+                }
+
+                textBox5.Text = Convert.ToString(cadena1.Length);
+                textBox8.Text = repetida;
+
+
+                //Sufijo
+                int lcadena1 = Convert.ToInt32(cadena1.Length);
+                int d = lcadena1 - 2;
+                int f = 2;
+                string sufijo = cadena1.Substring(lcadena1 - 1, 1);
+                string caracter2;
+
+                while (f <= lcadena1)
+                {
+                    caracter2 = cadena1.Substring(d, f);
+                    sufijo = sufijo + "," + caracter2;
+                    d--;
+                    f++;
+                }
+
+                textBox10.Text = "ε, " + sufijo;
+
+                //Prefijo
+                //cu = cadena1
+
+                int c = 2;
+                string prefijo = cadena1.Substring(0, 1);
+                string caracter;
+                while (c <= lcadena1)
+                {
+                    caracter = cadena1.Substring(0, c);
+                    prefijo = prefijo + ", " + caracter;
+                    c++;
+                }
+                textBox11.Text = "ε, " + prefijo;
+
+                //Subcadena
+                string s1 = textBox1.Text;
+                string s2 = textBox2.Text;
+
+                if (s1.Contains(s2))
+                {
+                    textBox12.Text = "Verdadero";
+                }
+                else
+                {
+                    textBox12.Text = "Falso";
+                }
+
+                //Invertir cadena
+                string c1 = textBox1.Text;
+                string c2 = textBox2.Text;
+                string Transponer(string cadena)
+                {
+                    string cadenaInvertida = "";
+                    // Recorrer cadena letra por letra
+                    foreach (char letra in cadena)
+                    {
+                        // Anteponer la letra a la cadena invertida
+                        cadenaInvertida = letra + cadenaInvertida;
+                    }
+                    return cadenaInvertida;
+                }
+                textBox13.Text = Transponer(c1);
+                textBox14.Text = Transponer(c2);
+
+                //palindromo
+                int i = 0, startchar, lastchar;
+
+                char[] character = new char[100];
+                character = low.ToCharArray();
+                startchar = 0;
+                lastchar = character.Length - 1;
+                while (startchar < lastchar)
+                {
+                    if (character[startchar] == character[lastchar])
+                    {
+                        startchar++;
+                        lastchar--;
+                    }
+                    else
+                    {
+                        textBox15.Text = "No es un Palindromo";
+                        i = 2;
+                        break;
+                    }
+                }
+                if (i < 2)
+                {
+                    textBox15.Text = "Es un Palindromo";
+
+                }
+                else
+                {
+                    i = 0;
+                }
+
+                /////////////////////////////////////////
+            }
+        
+
+    }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+            textBox11.Text = "";
+            textBox12.Text = "";
+            textBox13.Text = "";
+            textBox14.Text = "";
+            textBox15.Text = "";
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void textBox9_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Menu menu = new Menu();
+            menu.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+
+        private HashSet<string> conjuntoA = new HashSet<string>();
+        private HashSet<string> conjuntoB = new HashSet<string>();
+        private HashSet<string> unionAB = new HashSet<string>();
+        private HashSet<string> interseccionAB = new HashSet<string>();
+        private HashSet<string> diferenciaAB = new HashSet<string>();
+        private HashSet<string> diferenciaBA = new HashSet<string>();
+
+        ConjuntoDoble conjunto;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void obtenerElementosConjuntoA()
+        {
+            for (int i = 0; i < this.txtConjuntoA.Lines.Count(); i++)
+            {
+                this.conjuntoA.Add(this.txtConjuntoA.Lines[i]);
+            }
+        }
+
+        public void obtenerElementosConjuntoB()
+        {
+            for (int i = 0; i < this.txtConjuntoB.Lines.Count(); i++)
+            {
+                this.conjuntoB.Add(this.txtConjuntoB.Lines[i]);
+            }
+        }
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            obtenerElementosConjuntoA();
+            obtenerElementosConjuntoB();
+
+            this.txtConjuntoA.Enabled = false;
+            this.txtConjuntoB.Enabled = false;
+
+            conjunto = new ConjuntoDoble(this.conjuntoA, this.conjuntoB);
+            unionAB = conjunto.operarUnion();
+            interseccionAB = conjunto.operarInterseccion();
+            diferenciaAB = conjunto.operarDiferenciaAB();
+            diferenciaBA = conjunto.operarDiferenciaBA();
+            igualdades();
+            mostrarResultados();
+            this.btnOperar.Enabled = false;
+        }
+
+        public void mostrarResultados()
+        {
+
+
+            if (unionAB.Count == 0)
+            {
+                this.txtUnionAB.Text = "Conjunto Vacio";
+
+            }
+            else
+            {
+                foreach (string elemento in unionAB)
+                {
+                    this.txtUnionAB.Text += (elemento + ", ");
+                }
+            }
+
+            if (interseccionAB.Count == 0)
+            {
+                this.txtInterseccionAB.Text = "Conjunto Vacio";
+            }
+            else
+            {
+                foreach (string elemento in interseccionAB)
+                {
+                    this.txtInterseccionAB.Text += (elemento + ", ");
+                }
+            }
+
+            if (diferenciaAB.Count == 0)
+            {
+                this.txtDiferenciaAB.Text = "Conjunto Vacio";
+            }
+            else
+            {
+                foreach (string elemento in diferenciaAB)
+                {
+                    this.txtDiferenciaAB.Text += (elemento + ", ");
+                }
+            }
+
+            if (diferenciaBA.Count == 0)
+            {
+                this.txtDiferenciaBA.Text = "Conjunto Vacio";
+            }
+            else
+            {
+                foreach (string elemento in diferenciaBA)
+                {
+                    this.txtDiferenciaBA.Text += (elemento + ", ");
+                }
+            }
+
+
+
+            if (conjuntoA.SetEquals(conjuntoB))
+            {
+                label4.Text = "Los elementos de ambos conjuntos son iguales";
+            }
+            else
+            {
+
+                label4.Text = "Los elementos de ambos conjuntos son diferentes";
+            }
+
+
+
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+        public void limpiar()
+        {
+            this.txtConjuntoA.Clear();
+            this.txtConjuntoB.Clear();
+            this.txtUnionAB.Clear();
+            this.txtInterseccionAB.Clear();
+            this.txtDiferenciaAB.Clear();
+            this.txtDiferenciaBA.Clear();
+            this.txtConjuntoA.Enabled = true;
+            this.txtConjuntoB.Enabled = true;
+            this.btnOperar.Enabled = true;
+            this.conjuntoA.Clear();
+            this.conjuntoB.Clear();
+            this.unionAB.Clear();
+            this.txtConjuntoA.Focus();
+            this.label4.Text = "";
+            this.txtPotenciaA.Clear();
+            this.txtPotenciaB.Clear();
+            txtTamañoA.Text = "";
+            txtTamañoB.Text = "";
+        }
+
+        void igualdades()
+        {
+            int diferentesA = 0;
+            int similitudes = 0;
+            int diferentesB = 0;
+            String[] difA = new string[20];
+            String[] difB = new string[20];
+
+            for (int i = 0; i < this.conjuntoA.Count; i++)
+            {
+                Boolean igualdad = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (conjuntoA.ElementAt(i).Equals(conjuntoA.ElementAt(j)))
+                    {
+                        igualdad = true;
+                    }
+                }
+                if (igualdad == false)
+                {
+                    difA[diferentesA] = conjuntoA.ElementAt(i);
+                    diferentesA++;
+                }
+            }
+
+
+            for (int i = 0; i < this.conjuntoB.Count; i++)
+            {
+                Boolean igualdad = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (conjuntoB.ElementAt(i).Equals(conjuntoB.ElementAt(j)))
+                    {
+                        igualdad = true;
+                    }
+                }
+                if (igualdad == false)
+                {
+                    difB[diferentesB] = conjuntoB.ElementAt(i);
+                    diferentesB++;
+                }
+            }
+
+            for (int i = 0; i < diferentesA; i++)
+            {
+                Boolean igualdad = false;
+                for (int j = 0; j < diferentesB; j++)
+                {
+                    if (difA.ElementAt(i) != null)
+                    {
+                        if (difA.ElementAt(i) != null)
+                        {
+                            if (difA.ElementAt(i).Equals(difB.ElementAt(j)))
+                            {
+                                igualdad = true;
+                            }
+                        }
+                    }
+
+                }
+                if (igualdad == true)
+                {
+                    similitudes++;
+                }
+            }
+
+            if (diferentesB == diferentesA && diferentesA == similitudes)
+            {
+                txtPotenciaA.Text = "Los conjuntos son IGUALES";
+            }
+            else
+            {
+                txtPotenciaA.Text = "Los conjuntos NO son IGUALES";
+            }
+
+            txtTamañoA.Text = " " + diferentesA;
+            txtTamañoB.Text = " " + diferentesB;
+
+            String A2 = "(0)";
+            if (diferentesA == 1)
+            {
+                A2 = "(0) (" + difA[0] + ")";
+            }
+            if (diferentesA == 2)
+            {
+                A2 = "(0) (" + difA[0] + "," + difA[1] + ") (" + difA[0] + ") (" + difA[1] + ")";
+            }
+            if (diferentesA == 3)
+            {
+                A2 = "(0) (" + difA[0] + "," + difA[1] + "," + difA[2] + ") (" + difA[0] + "," + difA[1] + ") (" + difA[0] + "," + difA[2] + ") (" + difA[1] + "," + difA[2] + ") (" + difA[0] + ") (" + difA[1] + ") (" + difA[2] + ")";
+            }
+
+            if (diferentesA == 4)
+            {
+                A2 = "(0) (" + difA[0] + "," + difA[1] + "," + difA[2] + "," + difA[3] + ") (" + difA[0] + "," + difA[1] + "," + difA[2] + ")  (" + difA[0] + "," + difA[1] + "," + difA[3] + ") (" + difA[0] + "," + difA[2] + "," + difA[3] + ") (" + difA[1] + "," + difA[2] + "," + difA[3] + ") (" + difA[0] + "," + difA[1] + ") (" + difA[0] + "," + difA[2] + ") (" + difA[0] + "," + difA[3] + ") (" + difA[1] + "," + difA[2] + ") (" + difA[1] + "," + difA[3] + ") (" + difA[2] + "," + difA[3] + ") (" + difA[0] + ") (" + difA[1] + ") (" + difA[2] + ") (" + difA[3] + ")";
+            }
+
+            if (diferentesA == 5)
+            {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          //  11
+                A2 = "(0) (" + difA[0] + "," + difA[1] + "," + difA[2] + "," + difA[3] + "," + difA[4] + ") (" + difA[0] + "," + difA[1] + "," + difA[2] + "," + difA[3] + ")  (" + difA[0] + "," + difA[1] + "," + difA[2] + "," + difA[4] + ") (" + difA[0] + "," + difA[1] + "," + difA[3] + "," + difA[4] + ") (" + difA[0] + "," + difA[2] + "," + difA[3] + "," + difA[4] + ") (" + difA[1] + "," + difA[2] + "," + difA[3] + "," + difA[4] + ") (" + difA[0] + "," + difA[1] + "," + difA[2] + ") (" + difA[0] + "," + difA[1] + "," + difA[3] + ") (" + difA[0] + "," + difA[1] + "," + difA[4] + ") (" + difA[0] + "," + difA[2] + "," + difA[3] + ") (" + difA[0] + "," + difA[2] + "," + difA[4] + ") (" + difA[0] + "," + difA[3] + "," + difA[4] + ") (" + difA[1] + "," + difA[2] + "," + difA[3] + ") (" + difA[1] + "," + difA[2] + "," + difA[4] + ") (" + difA[1] + "," + difA[3] + "," + difA[4] + ") (" + difA[2] + "," + difA[3] + "," + difA[4] + ") (" + difA[0] + "," + difA[1] + ") (" + difA[0] + "," + difA[2] + ") (" + difA[0] + "," + difA[3] + ") (" + difA[0] + "," + difA[4] + ") (" + difA[1] + "," + difA[2] + ") (" + difA[1] + "," + difA[3] + ") (" + difA[1] + "," + difA[4] + ") (" + difA[2] + "," + difA[3] + ") (" + difA[2] + "," + difA[4] + ") (" + difA[3] + "," + difA[4] + ") (" + difA[0] + ") (" + difA[1] + ") (" + difA[2] + ") (" + difA[3] + ") (" + difA[4] + ")";
+            }
+            txtPotenciaA.Text = A2;
+            //MessageBox.Show("Diferenes en A: " + diferentesA + " -- Diferentes B: " + diferentesB + " -- Similitudes: " + similitudes);
+            String B2 = "(0)";
+            if (diferentesB == 1)
+            {
+                B2 = "(0) (" + difB[0] + ")";
+            }
+            if (diferentesB == 2)
+            {
+                B2 = "(0) (" + difB[0] + "," + difB[1] + ") (" + difB[0] + ") (" + difB[1] + ")";
+            }
+            if (diferentesB == 3)
+            {
+                B2 = "(0) (" + difB[0] + "," + difB[1] + "," + difB[2] + ") (" + difB[0] + "," + difB[1] + ") (" + difB[0] + "," + difB[2] + ") (" + difB[1] + "," + difB[2] + ") (" + difB[0] + ") (" + difB[1] + ") (" + difB[2] + ")";
+            }
+
+            if (diferentesB == 4)
+            {
+                B2 = "(0) (" + difB[0] + "," + difB[1] + "," + difB[2] + "," + difB[3] + ") (" + difB[0] + "," + difB[1] + "," + difB[2] + ")  (" + difB[0] + "," + difB[1] + "," + difB[3] + ") (" + difB[0] + "," + difB[2] + "," + difB[3] + ") (" + difB[1] + "," + difB[2] + "," + difB[3] + ") (" + difB[0] + "," + difB[1] + ") (" + difB[0] + "," + difB[2] + ") (" + difB[0] + "," + difB[3] + ") (" + difB[1] + "," + difB[2] + ") (" + difB[1] + "," + difB[3] + ") (" + difB[2] + "," + difB[3] + ") (" + difB[0] + ") (" + difB[1] + ") (" + difB[2] + ") (" + difB[3] + ")";
+            }
+
+            if (diferentesB == 5)
+            {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          //  11
+                B2 = "(0) (" + difB[0] + "," + difB[1] + "," + difB[2] + "," + difB[3] + "," + difB[4] + ") (" + difB[0] + "," + difB[1] + "," + difB[2] + "," + difB[3] + ")  (" + difB[0] + "," + difB[1] + "," + difB[2] + "," + difB[4] + ") (" + difB[0] + "," + difB[1] + "," + difB[3] + "," + difB[4] + ") (" + difB[0] + "," + difB[2] + "," + difB[3] + "," + difB[4] + ") (" + difB[1] + "," + difB[2] + "," + difB[3] + "," + difB[4] + ") (" + difB[0] + "," + difB[1] + "," + difB[2] + ") (" + difB[0] + "," + difB[1] + "," + difB[3] + ") (" + difB[0] + "," + difB[1] + "," + difB[4] + ") (" + difB[0] + "," + difB[2] + "," + difB[3] + ") (" + difB[0] + "," + difB[2] + "," + difB[4] + ") (" + difB[0] + "," + difB[3] + "," + difB[4] + ") (" + difB[1] + "," + difB[2] + "," + difB[3] + ") (" + difB[1] + "," + difB[2] + "," + difB[4] + ") (" + difB[1] + "," + difB[3] + "," + difB[4] + ") (" + difB[2] + "," + difB[3] + "," + difB[4] + ") (" + difB[0] + "," + difB[1] + ") (" + difB[0] + "," + difB[2] + ") (" + difB[0] + "," + difB[3] + ") (" + difB[0] + "," + difB[4] + ") (" + difB[1] + "," + difB[2] + ") (" + difB[1] + "," + difB[3] + ") (" + difB[1] + "," + difB[4] + ") (" + difB[2] + "," + difB[3] + ") (" + difB[2] + "," + difB[4] + ") (" + difB[3] + "," + difB[4] + ") (" + difB[0] + ") (" + difB[1] + ") (" + difB[2] + ") (" + difB[3] + ") (" + difB[4] + ")";
+            }
+            txtPotenciaB.Text = B2;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Menu menu = new Menu();
+            menu.Show();
+        }
+    }
+    
+}
